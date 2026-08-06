a33342c624ff84a4fe3dd8e69700c52ed3919b6d
AmilIruy <yuriaciole@hotmail.com>
2026-08-06 16:58:06 -0300

    Mapas

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [],
+  build: {
+    // Gera arquivos .map para depuração e para que o Lighthouse possa acessá-los.
+    // Alternativas: 'inline' (incluir no bundle) ou 'hidden' (gera mapas mas não adiciona sourceMappingURL).
+    sourcemap: true,
+  },
 })
